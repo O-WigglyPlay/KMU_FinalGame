42d19ee8e038d8e3d377b84a776c64052e632864
KTPotato <rnalgh1478069@gmail.com>
2024-05-14 12:49:18 +0900

    좀비

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] spawnPoint;
+
+    float timer;
+
+    void Awake()
+    {
+        spawnPoint = GetComponentsInChildren<Transform>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
