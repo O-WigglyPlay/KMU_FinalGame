561dd192f6c13ff5cf44935207ab07aefe56760d
오재준 <dhehfud00@naver.com>
2024-05-21 12:40:49 +0900

    UI,Player
    UI
    - 저장하기, 게임 시작되면 자동으로 불러오기 구현 (Json 사용)
    - BGM,Effect, 해상도 수정 가능
    Player
    - 새로운 캐릭터 이미지 파일 적용

---
+++ Assets/Scripts/Game/Data/GameSettings.cs
@@ -0,0 +1,7 @@
+[System.Serializable]
+public class GameSettings
+{
+    public float bgmVolume;       // BGM 볼륨 설정
+    public float effectVolume;    // 이펙트 사운드 볼륨 설정
+    public int resolutionIndex;   // 해상도 인덱스 설정
+}
