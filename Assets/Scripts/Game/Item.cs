dadc1eace1c2877ff63f58389e6f663367978ec7
Weeds4 <166020065+Weeds4@users.noreply.github.com>
2024-05-23 00:48:57 +0900

    .

--- Assets/Scripts/Game/Item.cs
+++ Assets/Scripts/Game/Item.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     public Sprite itemIcon;
     public SlotTag itemTag;
@@ -12,15 +13,21 @@ public class Item : MonoBehaviour
     [Header("If the item can be equipped")]
     public GameObject equipmentPrefab;
 >>>>>>> parent of c649bda (Merge pull request #17 from O-WigglyPlay/Fild)
+=======
+>>>>>>> parent of efb321a (Reapply "Ïù∏Î≤§ÌÜ†Î¶¨ ÏÑ±Í≥µ!")
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
 <<<<<<< HEAD
+<<<<<<< HEAD
             Destroy(gameObject); // æ∆¿Ã≈€¿Ã «√∑π¿ÃæÓøÕ √Êµπ«œ∏È æ∆¿Ã≈€¿ª ∆ƒ±´«’¥œ¥Ÿ.
 =======
             Destroy(gameObject);
 >>>>>>> parent of c649bda (Merge pull request #17 from O-WigglyPlay/Fild)
+=======
+            Destroy(gameObject); // æ∆¿Ã≈€¿Ã «√∑π¿ÃæÓøÕ √Êµπ«œ∏È æ∆¿Ã≈€¿ª ∆ƒ±´«’¥œ¥Ÿ.
+>>>>>>> parent of efb321a (Reapply "Ïù∏Î≤§ÌÜ†Î¶¨ ÏÑ±Í≥µ!")
         }
     }
 }
