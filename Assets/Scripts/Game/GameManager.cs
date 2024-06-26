14dc189ad551203fbfc9cfafaae1a6794d5e4087
Weeds4 <weeds6568@gmail.com>
2024-06-26 15:33:12 +0900

    게임씬 설정 버튼

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject menuSet;
+    public GameObject Player;
+
+
+    void Start()
+    {
+        GameLoad();
+
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+            menuSet.SetActive(true);
+    }
+
+
+    public void GameSave()
+    {
+        PlayerPrefs.SetFloat("PlayerX", Player.transform.position.x);
+        PlayerPrefs.SetFloat("PlayerY", Player.transform.position.y);
+        PlayerPrefs.Save();
+
+        menuSet.SetActive(false);
+    }
+
+    public void GameLoad()
+    {
+
+        if (PlayerPrefs.HasKey("PlayerX"))
+            return;
+
+        float x = PlayerPrefs.GetFloat("PayerX");
+        float y = PlayerPrefs.GetFloat("PayerY");
+
+        Player.transform.position = new Vector3(x, y, 0);
+
+    }
+
+
+    public void GameQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+
+}
